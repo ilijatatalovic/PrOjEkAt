871173acfec29e7c3bb7886df2939f1be0cfc38c
ilijatatalovic <63732221+ilijatatalovic@users.noreply.github.com>
2020-04-25 17:38:08 +0200

    Dodato nasledjivanje, dodati jos svasta nesto

--- projekat/osoblje.hpp
+++ projekat/osoblje.hpp
@@ -1,6 +1,54 @@
 #ifndef OSOBLJE_HPP_INCLUDED
 #define OSOBLJE_HPP_INCLUDED
 
+class Osoblje{
+  protected:
+int godine;
+string ime, prezime;
+ public:
+  Osoblje(int g, string i, string p)
+  {
+  godine=g;
+  ime=i;
+  prezime=p;
+  }
+  Osoblje()
+  {
+  godine=0;
+  ime="";
+  prezime="";
+  }
+  Osoblje(const osoblje& o)
+  {
+  godine=o.godine;
+  ime=o.ime;
+  prezime=o.prezime;
+  }
+ 
+  
+  
+  
+  
+  
+  
+  
+  class Doktor: public Osoblje
+  {
+  private:
+    bool medicina;
+  public:
+    Doktor():Osoblje("50","marko","kra;jevic"), medicina(true){}
+    Doktor(int g, string i, string prezime,int med):Osoblje(g,i,p),medicina(med){}
+    Doktor(const Doktor& d):Osoblje(s), medicina(s.medicina){} 
+    Doktor(const Osoblje& o):Osoblje(o), medicina(medi){} 
+  
+  };
+  
+  
+  
+  
+
+};
 
 
 #endif // OSOBLJE_HPP_INCLUDED
