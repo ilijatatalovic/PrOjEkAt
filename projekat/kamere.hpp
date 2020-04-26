ad802dece7af34c5a421e6ad036bb370eb5b825f
ilijatatalovic <63732221+ilijatatalovic@users.noreply.github.com>
2020-04-26 13:52:37 +0200

    Dodata polja

--- projekat/kamere.hpp
+++ projekat/kamere.hpp
@@ -1,6 +1,16 @@
 #ifndef KAMERE_HPP_INCLUDED
 #define KAMERE_HPP_INCLUDED
 
+class Kamere{
+  private:
+  bool slika;
+public: 
+  Kamere(bool slika){
+  slika=s;
+  
+  }
+
+};
 
 
 #endif // KAMERE_HPP_INCLUDED
