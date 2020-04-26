1a1622a37fa6021952321c2325f8089e192adb4f
ilijatatalovic <63732221+ilijatatalovic@users.noreply.github.com>
2020-04-26 13:28:43 +0200

    Dodata polja

--- projekat/tiptakm.hpp
+++ projekat/tiptakm.hpp
@@ -1,6 +1,20 @@
 #ifndef TIPTAKM_HPP_INCLUDED
 #define TIPTAKM_HPP_INCLUDED
 
+class TipTakmicenja{
+private:
+  string drzava;
+  string grad;
+  string nivoTakmicenja;
+public:
+  TipTakmicenja(string drzava,string grad,string nivoTakmicenja){
+  drzava=drz;
+  grad=grd;  
+  nivoTakmicenja=nTak;
+  }
+
+
+};
 
 
 #endif // TIPTAKM_HPP_INCLUDED
