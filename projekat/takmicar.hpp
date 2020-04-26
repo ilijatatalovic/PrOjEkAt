987483457e88e98fa93d9d7a13c5192d8a7a42eb
ilijatatalovic <63732221+ilijatatalovic@users.noreply.github.com>
2020-04-26 13:50:44 +0200

    Dodata polja

--- projekat/takmicar.hpp
+++ projekat/takmicar.hpp
@@ -1,6 +1,19 @@
 #ifndef TAKMICAR_HPP_INCLUDED
 #define TAKMICAR_HPP_INCLUDED
-
+#include "camac.hpp"
+#include "osoblje.hpp"
+class Takmicar: public Osoba{
+  private: 
+  Camac ;
+  Osoba ;
+  double vreme;
+  public:
+  Vreme(double vreme){
+  
+    vreme=V;
+  
+  }
+};
 
 
 #endif // TAKMICAR_HPP_INCLUDED
