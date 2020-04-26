1a0be80cfb3343aaa6924e85aae34dfc8ed581fa
ilijatatalovic <63732221+ilijatatalovic@users.noreply.github.com>
2020-04-26 13:40:24 +0200

    Dodata polja

--- projekat/takmicenje.hpp
+++ projekat/takmicenje.hpp
@@ -1,6 +1,19 @@
 #ifndef TAKMICENJE_HPP_INCLUDED
 #define TAKMICENJE_HPP_INCLUDED
-
+#include "osoblje.hpp"
+#include "tiptakm.hpp"
+#include "tabela.hpp"
+class Takmicenje{
+private:
+  Osoblje doktor;
+  TipTakmicenja ;
+  Tabela ;
+  string trka;
+public:
+  Takmicenje(string trka){
+    trka=t;
+  }
+};
 
 
 #endif // TAKMICENJE_HPP_INCLUDED
