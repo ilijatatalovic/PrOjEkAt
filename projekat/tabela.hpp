b46121c39a1cf14adcfab6c16a0438b483f970cc
ilijatatalovic <63732221+ilijatatalovic@users.noreply.github.com>
2020-04-26 13:22:12 +0200

    Dodata polja

--- projekat/tabela.hpp
+++ projekat/tabela.hpp
@@ -1,5 +1,19 @@
 #ifndef TABELA_HPP_INCLUDED
 #define TABELA_HPP_INCLUDED
+#include "osoblje.hpp"
+
+class Tabela{
+private:
+  Osoblje ;
+  int brojPolja;
+  double vreme;
+public: 
+  Tabela(int brojPolja,double vreme){
+  brojPolja=brP;
+  vreme=vr;
+  
+  }
+};
 
 
 
