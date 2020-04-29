8388a820477bd6086459768f1b3a9513270de4b0
ilijatatalovic <63732221+ilijatatalovic@users.noreply.github.com>
2020-04-29 20:56:22 +0200

    Update takmicar.hpp

--- projekat/takmicar.hpp
+++ projekat/takmicar.hpp
@@ -1,19 +1,38 @@
 #ifndef TAKMICAR_HPP_INCLUDED
 #define TAKMICAR_HPP_INCLUDED
-#include "camac.hpp"
-#include "osoblje.hpp"
-class Takmicar: public Osoba{
-  private: 
-  Camac ;
-  Osoba ;
+enum kategorija{pioniri=1, kadeti, juniori, seniori};
+class Takmicar{
+  protected: 
   double vreme;
+  string prezime;
+  string klub;
+  int godiste;
+  kategorija k;
   public:
-  Vreme(double vreme){
-  
+  Takmicar(double v, string p, string k, int g,kategorija k1){
     vreme=V;
+    prezime=p;
+    klub=k;
+    godiste=g;
+    k=k1;
+  }
+  void metoda(){
+  if(g>2016 || g<1980)
+  {
+  cout << "Au sinko pa ti se bodes : /" << endl;
+  }
+  if(g>2005)
+    k1=1;
+  if(g<2005 && g>2001)
+    k1=2;
+  if(g<2000 && g>1998)
+    k1=3;
+  if(g<1997)
+    k4=4;
   
   }
 };
+
 
 
 #endif // TAKMICAR_HPP_INCLUDED
