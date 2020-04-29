924f1566237c8d98bc82ee699524f341254a2875
ilijatatalovic <63732221+ilijatatalovic@users.noreply.github.com>
2020-04-29 21:18:56 +0200

    Update kamere.hpp

--- projekat/kamere.hpp
+++ projekat/kamere.hpp
@@ -1,14 +1,27 @@
 #ifndef KAMERE_HPP_INCLUDED
 #define KAMERE_HPP_INCLUDED
-
+enum rezolucija{144=1,360,480,1080};
 class Kamere{
-  private:
-  bool slika;
+  protected:
+  int daljina;
+  bool stanje;
+  rezolucija p;
 public: 
-  Kamere(bool s){
-  slika=s;
-  
+  Kamere(int d, bool sta, rezolucija p1){
+  daljina=d;
+  stanje=sta;
+  p=p1;
   }
+void metoda1(){
+if(stanje==true)
+  cout << "Upaljena kamera." << endl;
+ else
+   cout << "Ugasena kamera." << endl;
+if(p<4)
+  p=p+1;
+if(daljina<10)
+  p=p-1;
+}
 
 };
 
