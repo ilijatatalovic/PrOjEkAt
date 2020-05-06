0cb64ed565493671fb9848333d4e5b5092dfe474
ilijatatalovic <63732221+ilijatatalovic@users.noreply.github.com>
2020-05-06 18:21:47 +0200

    Update kamere.hpp

--- projekat/kamere.hpp
+++ projekat/kamere.hpp
@@ -1,20 +1,13 @@
 #ifndef KAMERE_HPP_INCLUDED
 #define KAMERE_HPP_INCLUDED
-<<<<<<< HEAD
 enum rezolucija{p144=1,p360,p480,p1080};
-=======
-enum rezolucija{144=1,360,480,1080};
->>>>>>> 33d36fbcbe836dbcb495a520b640673447fac27b
+
 class Kamere{
   protected:
   int daljina;
   bool stanje;
   rezolucija p;
-<<<<<<< HEAD
 public:
-=======
-public: 
->>>>>>> 33d36fbcbe836dbcb495a520b640673447fac27b
   Kamere(int d, bool sta, rezolucija p1){
   daljina=d;
   stanje=sta;
@@ -26,7 +19,6 @@ if(stanje==true)
  else
    cout << "Ugasena kamera." << endl;
 do{
-<<<<<<< HEAD
 p=p1080;
 }while(daljina> 10);
 do{
@@ -36,7 +28,6 @@ p=p+1;
 }while(daljina> 10);
 do{
 p=p-1;
->>>>>>> 33d36fbcbe836dbcb495a520b640673447fac27b
 }while(daljina< 5);
 
 }
